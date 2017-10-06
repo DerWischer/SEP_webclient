82a1d936fa5d52083c161e53f5de6b326dbcb2f4
Joshua <joshua.jungen@outlook.de>
2017-10-06 09:53:55 +0200

    Initial commit

---
+++ calle/WebPage/fileTree.js
@@ -0,0 +1,10 @@
+function Node(data) {
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+}
+ 
+function Tree(data) {
+    var node = new Node(data);
+    this._root = node;
+}
