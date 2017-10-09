9cc332f92eda8436c8c2e8d266478a59a4fafce8
Calle Persson <callepersson94@telia.com>
2017-10-09 22:17:08 +0200

    Functional Breadcrumb

--- js/calle.js
+++ js/calle.js
@@ -1,35 +1,68 @@
-var currentPath = [];
-
-
-function Node(data) {
-    this.data = data;
-    this.parent = null;
-    this.children = [];
+function findPathToRoot(startID ,inputFileSystem){
+	
+	console.log(startID);
+	
+	var currFile = inputFileSystem[startID];
+	var path = [[startID, currFile]];
+	
+	while(currFile["parent"] != null){
+		var nextID = currFile["parent"];
+		currFile = inputFileSystem[nextID];
+		path.unshift([nextID, currFile]);
+	}
+	return path;
 }
 
-function convertFileSystemToTree(fileSystem){
-	var root = null;
+function RenderBreadCrumbPath(path){
+	var breadCrumb = document.getElementById("breadcrumb");
+	
+	//Clear the breadcrumb
+	while ($("#breadcrumb > div").length > 2) {
+		breadCrumb.removeChild(breadCrumb.lastChild);
+	}
 
-	$.each(fileSystem, function(id, value) {
-		if (value == null) 
-			return;
+	for (var i = 0, len = path.length-1; i <= len; i++) {
+		//Skip if root
+		if(path[i][1].parent == null)
+			continue;
+		var breadCrumbElement = "<div class=\"parallelogram-breadcrumb btn btn-xs myBtn non-root-crumb \" id=\"breadcrumbIndex-"+i+"\" data-id=\""+path[i][0]+"\"> <span>"+path[i][1]["name"]+"</span> </div>";
+		breadCrumb.innerHTML += breadCrumbElement;
 		
-		var newNode = new Node(value);
-		newNode.parent = fileSystem[value["parent"]];
-		
-		for (i = 0; i < value["children"].length; i++) { 
-			newNode.children.push(fileSystem[(value["children"][i])]);
-		}
-		
-		newNode.data["id"] = id;
-		
-		if(newNode.parent == null) 
-			root = newNode;
+	}
+}
+
+$(document).ready(function() {
+	RenderBreadCrumbPath(findPathToRoot("a", filesystem));
+	
+	
+	$("#fileview").on("click", ".selectable", function(e){
+		var dataId = this.getAttribute("data-id");
+		RenderBreadCrumbPath(findPathToRoot(dataId, filesystem));
 	});
 	
-	return root;
-}
+	$(".breadcrumb-element").on("click", ".non-root-crumb", function(){
+		var dataId = this.getAttribute("data-id");
+		drawJSONexplorer(filesystem[dataId]);
+		RenderBreadCrumbPath(findPathToRoot(dataId, filesystem));
+		
+	});
+	
+	$(".breadcrumb-element").on("click", ".breadcrumb-base", function(){
+		drawJSONexplorer(filesystem[root]);
+		RenderBreadCrumbPath(findPathToRoot(root, filesystem));
+	});
+	
+	
+	
+});
 
-currentPath = [convertFileSystemToTree(filesystem)];
+var root = "b";
+
+
+
+
+
+
+
 
 
