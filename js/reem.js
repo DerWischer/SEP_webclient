267318a1f9ab5f9ef919d696c8ba5124966c6583
rim shahin <reemshahin@gmail.com>
2017-10-12 12:03:00 +0200

    onClick event

--- js/reem.js
+++ js/reem.js
@@ -5,6 +5,7 @@
 // <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
 
 
+
 $(document).ready(function() {
   var fileview = document.getElementById("fileview"); //equivalant to $("#fileview")[0];
   $(fileview).empty();
@@ -21,5 +22,9 @@ $(document).ready(function() {
   ul.appendChild(il2);
   fileview.appendChild(ul);
 
+  $(il1).on("click", function () {
+    setFolder(null);
+    console.log("set folder to null");
+  });
 
 });
