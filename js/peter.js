8d258e1540d5ee2707f68c3d4091559a24ae6185
Peter Pickerill <peterpickerill2016@gmail.com>
2017-10-12 19:00:56 +0200

    Made Clear btn work again

--- js/peter.js
+++ js/peter.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
 	$("#search-box").on('keyup', function (e) {
 		if ($(this).val().length == 0) {
+			$("#clear-btn").attr("disabled", true);
 			return;
 		}
+		$("#clear-btn").attr("disabled", false);
 		if (e.keyCode == 13) {
 		    CreateSearchCrumb($(this).val());
 		    searchFileSystem($(this).val());
 		}
+	});
+	$("#clear-btn").click(function() {
+		$("#search-box").val("").focus();
 	});
 });
 function searchFileSystem(m) {
