8fc29e49439b255b2c8dc8451cbba3ec5d54490e
sodasaft <emil.sundklev@gmail.com>
2017-11-07 22:51:01 +0100

    Put in the hacky version for translate that got removed before

--- static/translate/translator.js
+++ static/translate/translator.js
@@ -1,7 +1,7 @@
 var ENGLISH = 0;
 var SWEDISH = 1;
-function translatePageText(dictionary) {
-	var dictionary = dictionary || JSON.parse('{"Account":"Konto","Change Account":"Byt Konto","Logout":"Logga Ut","Login":"Logga In","Stop":"Sluta","Traces and Stuff":"Spårningar Och Saker","My Files":"Mina Filer","Shared With Me":"Delade Med Mig","Closed Files":"Stangda Filer","Opened Files":"Oppnade Filer","Last Modified Files":"Senast Andrade Filer","Trace":"Spåra","Notify Me Of Changes":"Meddela Mig Om Andringar","Close":"Stang","Name":"Namn","Date":"Datum","Build":"Bygg","Project":"Projekt","Design":"Design","Code":"Kod","Email":"Email","Customer":"Kund","Powder":"Pulver","Material":"Material","Printer":"Skrivare","File":"Fil","Files":"Filer","Build ID":"Bygg ID","Project ID":"Projekt ID","Design ID":"Design ID","Search":"Sok","Swedish":"Svenska","English":"Engelska"}');
+function translatePageText() {
+	var dictionary = SWEDISH_LANGUAGE;
 	$("body .translate").each(function() {
 		$(this).text(dictionary[$(this).text().trim()]);
 	});
@@ -16,7 +16,7 @@ function translatePage(language) {
 			languageFile = "languages/english.json"; 
 			break;
 		case SWEDISH:
-			languageFile = "languages/swedish.json"; 
+			languageFile = "languages/swedish.js";
 			break;
 	}
 	$.ajax({
