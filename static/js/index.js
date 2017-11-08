b51460c8b1ff0ac03dbab96b0549fdd873d043b6
Ionman64 <PeterPickerill2014@gmail.com>
2017-11-08 20:17:19 +0100

    combined js files into one

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,302 @@
+//calle
+//Returns an array [[ID, FileInPath], ...] containing the steps necessary to reach root from 'startID'
+function findPathToRoot(startID ,inputFileSystem){
+	var currFile = inputFileSystem[startID];
+	var path = [[startID, currFile]];
+
+	while(currFile["parent"] != null){
+		var nextID = currFile["parent"];
+		currFile = inputFileSystem[nextID];
+		path.unshift([nextID, currFile]);
+	}
+	return path;
+}
+
+//Draws the breadcrumb with the given path. INPUT FORMAT: [[RootID, RootFile], ... , [LastID, LastFile]]
+function RenderBreadCrumbPath(id){
+	var holder = $("#breadcrumb");
+	$(holder).empty();
+	breadcrumbs = [];
+	var currentFolder = id;
+	while (currentFolder != ROOT) {
+		var breadcrumb = el("div", {"class":"custom-breadcrumb btn btn-xs non-root-crumb", "data-id":currentFolder});
+		var span = el("span", {html:filesystem[currentFolder].name});
+		breadcrumb.appendChild(span);
+		breadcrumb.oncontextmenu = function(e) {
+			console.log(this);
+			MoveDropdownItemsToElement(this);
+			e.preventDefault();
+		}
+		breadcrumbs.unshift(breadcrumb);
+		currentFolder = getParent(currentFolder).id;
+	}
+	//Remove the root
+	//breadcrumbs.shift();
+	$.each(breadcrumbs, function(key, value) {
+		holder.append(value);
+	});
+}
+
+
+function MoveDropdownItemsToElement(inputElement){
+	createDropdownMenu(inputElement);
+	var rect = inputElement.getBoundingClientRect();
+	var recycledDropdown = document.getElementById('breadcrumb-dropdown');
+	
+	
+	
+	
+	recycledDropdown.style.left = (rect.left)+"px";
+	recycledDropdown.style.top = (rect.bottom)+"px";
+	
+	if(recycledDropdown.childElementCount > 0)
+		recycledDropdown.style.display = "block";
+	else
+		recycledDropdown.style.display = "none";
+	
+		
+	
+	
+}
+
+function createDropdownMenu(elem){
+	var id = elem.getAttribute('data-id');
+	var newElem = [];
+	$.each(getChildren(id), function(key, value){
+		if (value.type != "folder") {
+			return;
+		}
+		var listTag = el("li", {"class":"dropdown-element", "data-id":value.id});
+		var tag = el("a", {"href":"#", html:value.name});
+		listTag.appendChild(tag);
+		newElem.push(listTag);
+	});
+	
+	var holder = $("#breadcrumb-dropdown");
+	$(holder).empty();
+	
+	$.each(newElem, function(key, value) {
+		holder.append(value);
+	});	
+}
+
+function HideDropdownElement(){
+	var recycledDropdown = document.getElementById('breadcrumb-dropdown');
+	recycledDropdown.style.display = "none";
+}
+
+function CreateSearchCrumb(searchTerm){
+	var holder = $("#breadcrumb");
+	$(holder).empty();
+	var breadcrumb = el("div", {"class":"custom-breadcrumb btn btn-xs non-root-crumb", "data-id":ROOT});
+	var span = el("span", {html:"\""+searchTerm+"\""});
+	breadcrumb.appendChild(span);
+	holder.append(breadcrumb);
+}
+
+
+
+$(document).ready(function() {
+	
+	$(document).click(function() {
+		HideDropdownElement();
+	});
+	
+	//navtree is clicked
+	$("#fileview").on("click", ".selectable", function(e){
+		var dataId = this.getAttribute("data-id");
+		setFolder(dataId);
+	});
+	//root folder is clicked
+	$("#root-nav").on("click", function(){
+		setFolder(ROOT);
+	});
+	//root folder is clicked
+	$("#breadcrumb").on("click", ".non-root-crumb", function(){
+		setFolder(this.getAttribute("data-id"));
+	});
+	//breadcrumb dropdown element is clicked
+	$("#breadcrumb-dropdown").on("click", ".dropdown-element", function(){
+		console.log(this.getAttribute("data-id"));
+		setFolder(this.getAttribute("data-id"));
+	});
+	
+	
+	$("#breadcrumb .custom-breadcrumb").on("click", function(){
+		var id = this.getAttribute('data-id');
+		setFolder(id);
+	});
+	var rootCrumb = document.getElementById('root-nav');
+	rootCrumb.oncontextmenu = function(e) {
+		createDropdownMenu(rootCrumb);
+		MoveDropdownItemsToElement(this);
+		e.preventDefault();
+	}
+	
+	RenderBreadCrumbPath(ROOT);
+});
+//peter
+$(document).ready(function() {
+	$("#search-box").on('keyup', function (e) {
+		if ($(this).val().length == 0) {
+			$("#clear-btn").attr("disabled", true);
+			return;
+		}
+		$("#clear-btn").attr("disabled", false);
+		if (e.keyCode == 13) {
+		    CreateSearchCrumb($(this).val());
+		    searchFileSystem($(this).val());
+		}
+	});
+	$("#clear-btn").click(function() {
+		$("#search-box").val("").focus();
+		$("#clear-btn").attr("disabled", true);
+	});
+});
+function searchFileSystem(m) {
+	var found = [];
+	$.each(filesystem, function(key, value) {
+		if (value.name.indexOf(m) > -1) {
+			found.push(value);
+		}
+	});
+	drawJSONexplorer(null, found);
+	displayList(null, found);
+}
+//emil
+function displayList(parent, childrenObjects) {
+    var holder = $("#tableView")[0];
+    $(holder).empty();
+    var iconName;
+    $.each(childrenObjects, function(id, value) {
+        var row = el("section", {"data-id":value.id, class:"row listViewItem"});
+        if (value.type == "folder") {
+            iconName = "fa fa-folder-open fa-3x";
+        }
+        if (value.type == "file") {
+            iconName = "fa fa-file-o fa-3x";
+        }
+        var icon = el("section", {class:"col-lg-1 col-md-1"});
+        var fa = el("i", {class:iconName});
+        icon.appendChild(fa);
+        var name = el("section", {class:"col-lg-6 col-md-6"});
+        var h4 = el("h4", {html:value.name});
+        name.appendChild(h4);
+        var date = el("section", {class:"col-lg-5 col-md-5"});
+        var h4 = el("h4", {html:moment(value.lastModified*1000).format("YYYY-MM-DD")});
+        date.appendChild(h4);
+        row.appendChild(icon);
+        row.appendChild(name);
+        row.appendChild(date);
+        holder.appendChild(row);
+    });
+}
+
+// Login functions etc.
+var userData = {
+    "ragnar": {
+        "id":"ragnar",
+        "password":"lol"
+    },
+    "roger": {
+        "id":"roger",
+        "password":"hello"
+    }
+}
+function correctPW(userName, inputPassword) {
+    var user = userData.hasOwnProperty(userName);
+    if(user == true) {
+        var pw = userData[userName].password;
+        if(inputPassword == pw ) {
+            alert("yay");
+        }
+        else {
+            alert("incorrect pw")
+        }
+    }
+    if(user == false) {
+        alert("incorrect username");
+    }
+}
+
+
+(function($, window){
+
+    $.fn.contextMenu = function(settings) {
+        return this.each(function() {
+            // Code Goes Here
+            $(this).on("contextmenu", function (e) {
+                var $menu = $(settings.menuSelector)
+                    .data("invokedOn", $(e.target))
+                    .show()
+                    .css({
+                        position: "absolute",
+                        left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
+                        top: getMenuPosition(e.clientY, 'height', 'scrollTop')
+                    })
+                    .off('click')
+                    .on('click', 'a', function (e) {
+                        $menu.hide();
+
+                        var $invokedOn = $menu.data("invokedOn");
+                        var $selectedMenu = $(e.target);
+
+                        settings.menuSelected.call(this, $invokedOn, $selectedMenu);
+                    });
+
+                return false;
+            });
+            //make sure menu closes on any click
+            $('body').click(function () {
+                $(settings.menuSelector).hide();
+            });
+        });
+
+        function getMenuPosition(mouse, direction, scrollDir) {
+            var win = $(window)[direction](),
+                scroll = $(window)[scrollDir](),
+                menu = $(settings.menuSelector)[direction](),
+                position = mouse + scroll;
+
+            // opening menu would pass the side of the page
+            if (mouse + menu > win && menu < mouse)
+                position -= menu;
+
+            return position;
+        }
+    };
+
+})(jQuery, window);
+
+
+$(document).ready(function() {
+    //Handles menu drop down
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
+    //Logout button
+    $("#btnLogout").click(function() {
+        //alert("Value: " + $("#emailInput").val() + " Password: " + $("#passwordInput").val());
+        correctPW($("#emailInput").val(), $("#passwordInput").val());
+    });
+    $("#tableView").on("click", ".listViewItem", function() {
+        setFolder(this.getAttribute("data-id"));
+    });
+    $("#tableView").contextMenu({
+        menuSelector: "#contextMenu",
+        menuSelected: (function (invokedOn, selectedMenu) {
+            // context menu clicked
+            var msg = "You selected the menu item '" + selectedMenu.text() +
+                "' on the value '" + invokedOn.text() + "'";
+            if (selectedMenu.text() == "Trace") {
+                alert(msg);
+               $("#traceModal").modal();
+            }
+        })
+    });
+
+});
+//reem
 /* =========================================================
  * reem.js v1.0.0
  * =========================================================
@@ -531,3 +830,5 @@ function renderTraceTree(id)
 	  setFolder($(this).attr("data-id"));
 	  }) 
  }); */
+//joshua
+
